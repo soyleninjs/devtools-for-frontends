53e2e46013b49c145a0f1a75b41e7e6c3b676c68
soyleninjs <leninfelix@hotmail.com>
2024-10-27 23:32:33 -0600

    update main config

--- postcss.config.js
+++ postcss.config.js
@@ -1,33 +1,38 @@
 const config = {
   plugins: {
-    "postcss-functions": {
+    'postcss-functions': {
       functions: {
-        formula(minValue, maxValue, minViewportWidth, maxViewportWidth) {
+        formula (minValue, maxValue, minViewportWidth, maxViewportWidth) {
           // Modo de uso: formula(10, 20, 1024, 1440)
-          const calc = `calc(${minValue}px + (${maxValue} - ${minValue}) * ((100vw - ${minViewportWidth}px) / ${maxViewportWidth - minViewportWidth}))`;
+          const calc = `calc(${minValue}px + (${maxValue} - ${minValue}) * ((100vw - ${minViewportWidth}px) / ${maxViewportWidth - minViewportWidth}))`
 
-          return calc;
+          return calc
         },
-        responsive(minValue, maxValue, minViewportWidth = 1024, maxViewportWidth = 1440, returnNegativeValues = false) {
+        responsive (minValue, maxValue, minViewportWidth = 1024, maxViewportWidth = 1440, returnNegativeValues = false) {
           // Modo de uso: responsive(10, 20, 1024, 1440)
-          const clamp = `clamp(${minValue}px, calc(${minValue}px + (${maxValue} - ${minValue}) * ((100vw - ${minViewportWidth}px) / ${maxViewportWidth - minViewportWidth})), ${maxValue}px)`;
+          const clamp = `clamp(${minValue}px, calc(${minValue}px + (${maxValue} - ${minValue}) * ((100vw - ${minViewportWidth}px) / ${maxViewportWidth - minViewportWidth})), ${maxValue}px)`
 
           if (returnNegativeValues) {
-            return `calc(${clamp} * -1)`;
+            return `calc(${clamp} * -1)`
           }
 
-          return clamp;
+          return clamp
         },
-      },
+        bp (minValue, maxValue, mediaQueryChange = 680) {
+          // Modo de uso: bp(10, 20, 1024)
+          return `clamp(${minValue}px, calc(${minValue}px + (${maxValue} - ${minValue}) * (100vw - ${mediaQueryChange}px)), ${maxValue}px)`
+        }
+      }
     },
-    "postcss-nesting": {},
-    "postcss-sorting": {
-      order: ["custom-properties", "dollar-variables", "declarations", "at-rules", "rules"],
-      "unspecified-properties-position": "bottom",
+    'postcss-import': {},
+    'postcss-nesting': {},
+    'postcss-sorting': {
+      order: ['custom-properties', 'dollar-variables', 'declarations', 'at-rules', 'rules'],
+      'unspecified-properties-position': 'bottom'
     },
     'postcss-sort-media-queries': {
-      sort: 'desktop-first',
-    },
+      sort: 'desktop-first'
+    }
   },
 };
 
